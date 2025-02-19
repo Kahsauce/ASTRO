bde8b91143a9ae89a2e53f276a4b462945b75c72
Adrien Wyer <adrien.wyer@gmail.com>
2025-02-19 12:03:02 +0000

    Ajout de tree pour afficher la structure du projet

--- setup.sh
+++ setup.sh
@@ -4,8 +4,13 @@ echo "🔧 Mise à jour du système..."
 apt update && apt upgrade -y
 apt install -y nano git npm python3-pip curl wget unzip
 
+# Installation de tree pour voir la structure des fichiers
+apt update && apt install -y tree
+
+
 echo "🐍 Installation des dépendances Python..."
 pip install --upgrade pip
 pip install -r /mnt/ASTRO/requirements.txt
 
 echo "✅ Installation terminée !"
+
